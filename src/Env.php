c15d21593e696cbe07bb511ae1f14fcd211e3f7f
Adam Campbell <adam@hotmeteor.com>
2020-09-22 14:26:37 -0500

    Initial commit

---
+++ src/Env.php
@@ -0,0 +1,159 @@
+<?php
+
+
+namespace Eco;
+
+class Env
+{
+
+
+    /**
+     * Check if .env has a variable by key.
+     *
+     * @param $file
+     * @param $key
+     * @return bool
+     */
+    public static function has($file, $key): bool
+    {
+        $key = strtoupper(trim($key));
+
+        if (file_exists($file)) {
+            $lines = explode(PHP_EOL, file_get_contents($file));
+
+            foreach ($lines as $line) {
+                if (substr($line, 0, strlen($key)) === $key) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Get an .env variable by key.
+     *
+     * @param $file
+     * @param $key
+     * @return string
+     */
+    public static function get($file, $key)
+    {
+        $key = strtoupper(trim($key));
+
+        if (file_exists($file)) {
+            $lines = explode(PHP_EOL, file_get_contents($file));
+
+            foreach ($lines as $line) {
+                if (substr($line, 0, strlen($key)) === $key) {
+                    return $line;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Set an .env variable by key
+     *
+     * @param $file
+     * @param $key
+     * @param $value
+     */
+    public static function set($file, $key, $value): void
+    {
+        if (!file_exists($file)) {
+            file_put_contents($file, null);
+        }
+
+        $key = strtoupper(trim($key));
+
+        $setting = $key . '=' . self::formatValue($value) . PHP_EOL;
+
+        $temp_file = "{$file}.tmp";
+
+        $reading = fopen($file, 'r');
+        $writing = fopen($temp_file, 'w');
+
+        $replaced = false;
+
+        while (!feof($reading)) {
+            $line = fgets($reading);
+            if (substr($line, 0, strlen($key)) === $key) {
+                $line = $setting;
+                $replaced = true;
+            }
+
+            fputs($writing, $line);
+        }
+
+        fclose($reading);
+        fclose($writing);
+
+        if (!$replaced) {
+            file_put_contents($temp_file, $setting, FILE_APPEND);
+        }
+
+        rename($temp_file, $file);
+    }
+
+    /**
+     * Unset an .env variable by key
+     *
+     * @param $file
+     * @param $key
+     */
+    public static function unset($file, $key): void
+    {
+        if (!file_exists($file)) {
+            return;
+        }
+
+        $key = strtoupper(trim($key));
+
+        $temp_file = "{$file}.tmp";
+
+        $reading = fopen($file, 'r');
+        $writing = fopen($temp_file, 'w');
+
+        $replaced = false;
+
+        while (!feof($reading)) {
+            $line = fgets($reading);
+            if (substr($line, 0, strlen($key)) === $key) {
+                $line = '';
+                $replaced = true;
+            }
+
+            fputs($writing, $line);
+        }
+
+        fclose($reading);
+        fclose($writing);
+
+        if (!$replaced) {
+            unlink($temp_file);
+        } else {
+            rename($temp_file, $file);
+        }
+    }
+
+    /**
+     * Format an .env key name
+     *
+     * @param $value
+     * @return string
+     */
+    public static function formatValue($value)
+    {
+        $value = trim($value);
+
+        if (str_contains($value, ' ')) {
+            $value = "'" . $value . "'";
+        }
+
+        return $value;
+    }
+}
---
+++ tests/EnvTest.php
@@ -0,0 +1,82 @@
+<?php
+
+
+namespace Eco\Tests;
+
+use Eco\Env;
+use PHPUnit\Framework\TestCase;
+
+class EnvTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $content =
+        "FIRST_KEY=first".PHP_EOL.
+        "SECOND_KEY=2".PHP_EOL.
+        "LAST_KEY=".PHP_EOL;
+
+        file_put_contents('.env', $content);
+    }
+
+    public function test_has()
+    {
+        $first = Env::has('.env', 'FIRST_KEY');
+        $second = Env::has('.env', 'second_key');
+        $last = Env::has('.env', 'Last_Key');
+        $invalid = Env::has('.env', 'INVALID');
+
+        $this->assertTrue($first);
+        $this->assertTrue($second);
+        $this->assertTrue($last);
+        $this->assertFalse($invalid);
+    }
+
+    public function test_get()
+    {
+        $first = Env::get('.env', 'FIRST_KEY');
+        $second = Env::get('.env', 'second_key');
+        $last = Env::get('.env', 'Last_Key');
+        $invalid = Env::get('.env', 'INVALID');
+
+        $this->assertSame('FIRST_KEY=first', $first);
+        $this->assertSame('SECOND_KEY=2', $second);
+        $this->assertSame('LAST_KEY=', $last);
+        $this->assertNull($invalid);
+    }
+
+    public function test_set()
+    {
+        Env::set('.env', 'FIRST_KEY', 'zim');
+        Env::set('.env', 'second_key', 'zala');
+        Env::set('.env', 'Last_Key', 'bim');
+        Env::set('.env', 'NEW', 'new with space');
+
+        $this->assertStringContainsString('FIRST_KEY=zim'.PHP_EOL, file_get_contents('.env'));
+        $this->assertStringContainsString('SECOND_KEY=zala'.PHP_EOL, file_get_contents('.env'));
+        $this->assertStringContainsString('LAST_KEY=bim'.PHP_EOL, file_get_contents('.env'));
+        $this->assertStringContainsString('NEW=\'new with space\''.PHP_EOL, file_get_contents('.env'));
+    }
+
+    public function test_unset()
+    {
+        Env::unset('.env', 'FIRST_KEY');
+        Env::unset('.env', 'second_key');
+        Env::unset('.env', 'Last_Key');
+        Env::unset('.env', 'NEW');
+
+        $this->assertStringNotContainsString('FIRST_KEY=first'.PHP_EOL, file_get_contents('.env'));
+        $this->assertStringNotContainsString('SECOND_KEY=2'.PHP_EOL, file_get_contents('.env'));
+        $this->assertStringNotContainsString('LAST_KEY='.PHP_EOL, file_get_contents('.env'));
+        $this->assertStringNotContainsString('NEW='.PHP_EOL, file_get_contents('.env'));
+    }
+
+    public function test_format_value()
+    {
+        $this->assertSame('value', Env::formatValue('value'));
+        $this->assertSame('value', Env::formatValue('  value  '));
+        $this->assertSame("'the value'", Env::formatValue('the value'));
+        $this->assertSame("'the value'", Env::formatValue('  the value  '));
+    }
+}
